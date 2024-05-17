dea2de9d1098229dc7efda8c3cf0c5b6d37043c2
Gabriel Faria <gabrielfaria88@gmail.com>
2024-05-16 21:34:52 -0300

    creating buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Button from "./components/Button";
 import Input from "./components/Input";
-import { Container, Content } from "./styles";
+import { Container, Content, Row, Column } from "./styles";
 
 const App = () => {
   return (
     <Container>
       <Content>
         <Input />
+          <Row>
+            <Button label='7'/>
+            <Button label='8'/>
+            <Button label='8'/>
+            <Button label='C'/>
+          </Row>
+          <Row>
+            <Button label='4'/>
+            <Button label='5'/>
+            <Button label='6'/>
+            <Button label='+'/>
+          </Row>
+          <Row>
+            <Button label='1'/>
+            <Button label='2'/>
+            <Button label='3'/>
+            <Button label='-'/>
+          </Row>
+          <Row>
+            <Button label='.'/>
+            <Button label='0'/>
+            <Button label='X'/>
+            <Button label='/'/>
+          </Row>
       </Content>
     </Container>
   );
