bf9a8e42d74e2ca8e12ae7aee43f0b1aefcc0e20
Gabriel Faria <gabrielfaria88@gmail.com>
2024-05-19 11:41:48 -0300

    creating multiplication/division function

--- src/App.js
+++ src/App.js
@@ -42,6 +42,30 @@ const App = () => {
     }
   }
   
+  const handleMultiplication = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('*')
+    } else {
+      const multiply = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(multiply));
+      setOperation('');
+    }
+  }
+  
+  const handleDivision = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/')
+    } else {
+      const divide = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(divide));
+      setOperation('');
+    }
+  }
+  
   const handleEquals = () => {
     if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
       switch (operation) {
@@ -50,6 +74,12 @@ const App = () => {
           break;
         case '-':
           handleSubtract()
+          break;
+        case '*':
+          handleMultiplication()
+          break;
+        case '/':
+          handleDivision();
           break;
         default:
           break;
@@ -65,25 +95,25 @@ const App = () => {
             <Button label='7' onClick={() => handleAddNumber('7')}/>
             <Button label='8' onClick={() => handleAddNumber('8')}/>
             <Button label='9' onClick={() => handleAddNumber('9')}/>
-            <Button label='C' onClick={handleClear}/>
+            <Button label='+' onClick={handleSum}/>
           </Row>
           <Row>
             <Button label='4' onClick={() => handleAddNumber('4')}/>
             <Button label='5' onClick={() => handleAddNumber('5')}/>
             <Button label='6' onClick={() => handleAddNumber('6')}/>
-            <Button label='+' onClick={handleSum}/>
+            <Button label='-' onClick={handleSubtract}/>
           </Row>
           <Row>
             <Button label='1' onClick={() => handleAddNumber('1')}/>
             <Button label='2' onClick={() => handleAddNumber('2')}/>
             <Button label='3' onClick={() => handleAddNumber('3')}/>
-            <Button label='-' onClick={handleSubtract}/>
+            <Button label='x' onClick={handleMultiplication}/>
           </Row>
           <Row>
             <Button label='=' onClick={handleEquals}/>
             <Button label='0' onClick={() => handleAddNumber('0')}/>
-            <Button label='X'/>
-            <Button label='/'/>
+            <Button label='C' onClick={handleClear}/>
+            <Button label='/' onClick={handleDivision}/>
           </Row>
       </Content>
     </Container>
